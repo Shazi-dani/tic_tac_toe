b9c70ae82dbf6c08092e83c285edadbc3b73ecaf
shazina danish <shazinamunir26@gmail.com>
2023-08-24 21:25:05 +0000

    add a function playerinput for getting valid data from user and select a spot on board

--- run.py
+++ run.py
@@ -20,6 +20,26 @@ def printBoard(board):
     
 
 #take player input
+    """
+    this function get intergers from 1 to 9 
+    (which showes 9 places in a game board.)
+    as a input and also check validation of input.
+
+    """
+def playerInput(board):
+    print("Enter a number form 1 to 9.")
+    print("Each number represent a place on game board")
+    print("if you eneter 1 number its shows you selcet first place on the board and so on...")
+    global currentPlayer
+    inp = int(input("Select a spot 1-9: "))
+    if inp <= 0 or inp >=10:
+         print("Invalid number...Input must be 1 to 9")
+    elif inp >= 1 and inp <=9 and board[inp-1] != "-":
+         print("Oops player is already in that spot!")
+    else:
+        board[inp-1] = currentPlayer
+
+
 
 #check for win or tie
 
@@ -29,5 +49,10 @@ def printBoard(board):
 
 
 # run the game
-
-printBoard(board)
+"""
+    Run a while loop to collect a valid data from the user
+    via the terminal, and run all program functions
+"""
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
