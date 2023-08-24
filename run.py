a7e31ff30c8c828b4b68207c7ead3e93f9d4caa2
shazina danish <shazinamunir26@gmail.com>
2023-08-24 23:28:27 +0000

    define checkrow function for checking all rows of board if any row have same data then return true

--- run.py
+++ run.py
@@ -11,6 +11,9 @@ gameRunning = True
 
 
 #game board printing
+"""
+define a function for desiging and printing game board
+"""
 def printBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("---------")
@@ -20,13 +23,13 @@ def printBoard(board):
     
 
 #take player input
+  
+def playerInput(board):
     """
     this function get intergers from 1 to 9 
     (which showes 9 places in a game board.)
     as a input and also check validation of input.
-
     """
-def playerInput(board):
     print("Enter a number form 1 to 9.")
     print("Each number represent a place on game board")
     print("if you eneter 1 number its shows you selcet first place on the board and so on...")
@@ -42,6 +45,31 @@ def playerInput(board):
 
 
 #check for win or tie
+"""
+Check if player won using the winning combinations or tie.
+there is There are a total of 8 ways to arrange the same sign and win the game.
+3 in rows,3 in colums and 2 diagonoly.so define 3 different function for checking all possiblities.
+"""
+
+def checkrows(board):
+    """
+    define a function to check if all the signs are same
+    in any row in the board.
+    """
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != "-":
+        winner = board[0]
+        return True
+
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True     
+
+       
 
 #switch the player
 
@@ -56,3 +84,4 @@ def playerInput(board):
 while gameRunning:
     printBoard(board)
     playerInput(board)
+    
